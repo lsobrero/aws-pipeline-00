d615f76d1645917fd1ba522878a0f3844954d21d
luis sobrero <lsobrero@gmail.com>
2022-12-06 19:22:09 +0100

    SQS SNS example complted

--- lib/aws-pipeline-00-stack.ts
+++ lib/aws-pipeline-00-stack.ts
@@ -1,19 +1,20 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
 import * as sns from 'aws-cdk-lib/aws-sns';
 import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Construct } from 'constructs';
 
-export class AwsPipeline00Stack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class AwsPipeline00Stack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'AwsPipeline00Queue', {
-      visibilityTimeout: Duration.seconds(300)
+    const queue = new sqs.Queue(this, "CdkWorkshopQue", {
+      visibilityTimeout: cdk.Duration.seconds(300)
     });
 
-    const topic = new sns.Topic(this, 'AwsPipeline00Topic');
+    const topic = new sns.Topic(this, "CdkWorkshopTopic", {});
 
     topic.addSubscription(new subs.SqsSubscription(queue));
+
   }
 }

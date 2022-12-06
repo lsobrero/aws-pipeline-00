5564f5710deaac2b906637fe0ea50b790b1a8f86
luis sobrero <lsobrero@gmail.com>
2022-12-06 19:47:14 +0100

    Lambda example completed

--- lib/aws-pipeline-00-stack.ts
+++ lib/aws-pipeline-00-stack.ts
@@ -1,19 +1,20 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway'
 
 export class AwsPipeline00Stack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'AwsPipeline00Queue', {
-      visibilityTimeout: Duration.seconds(300)
+    const hello = new lambda.Function(this, 'hello', {
+      runtime: lambda.Runtime.NODEJS_14_X,    // execution environment
+      code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
+      handler: 'hello.handler'                // file is "hello", function is "handler"
     });
 
-    const topic = new sns.Topic(this, 'AwsPipeline00Topic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    new apigw.LambdaRestApi(this,'EndPoint',{
+      handler: hello
+    });
   }
 }
